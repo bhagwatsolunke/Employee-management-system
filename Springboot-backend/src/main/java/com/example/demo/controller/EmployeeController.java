a15daae0aab57fc0f73c746ea47d975de6ae69cf
bhagwatsolunke <bhagwatsolunke007@gmail.com>
2023-10-08 20:10:34 +0530

    update employee details by id,rest api created

--- Springboot-backend/src/main/java/com/example/demo/controller/EmployeeController.java
+++ Springboot-backend/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,4 +43,29 @@ public class EmployeeController {
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + id));
         return ResponseEntity.ok(employee); // Fixed the typo here
     }
+    
+    //update employee api
+    
+    @PutMapping("/employee/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+    	 Employee employee = employeeRepository.findById(id)
+                 .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id: " + id));
+    employee.setFirstName(employeeDetails.getFirstName());
+    employee.setLastName(employeeDetails.getLastName());
+    employee.setEmailId(employeeDetails.getEmailId());
+    
+    Employee updateEmployee = employeeRepository.save(employee);
+    return ResponseEntity.ok(updateEmployee);
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
